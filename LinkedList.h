2117b3c796191e3a3005879cb4f2bac63c2ff25b
Duncan Reeves <duncansreeves@gmail.com>
2018-03-26 15:53:05 -0600

    Next

--- LinkedList.h
+++ LinkedList.h
@@ -7,13 +7,15 @@ class ListNode
 {
 public:
     ListNode();
+    ListNode(string name, Type data);
     ~ListNode();
     //Needs copy constructor
 private:
     ListNode<Type>* front;
     ListNode<Type>* back;
 
-    string 
+    string name;
+    Type data;
 };
 
 template <class Type>
@@ -23,7 +25,10 @@ public:
     LinkedList();
     ~LinkedList();
 
-    void add();
+    void add(string name, Type data);
+    void add(ListNode<Type>& node);
+    
+    //overloaded + operator here
 private:
     ListNode<Type>* head;
 };
