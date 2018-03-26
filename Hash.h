1d1a3878083232f577ea725aee91c2bf321f2baa
slamer9 <duncansreeves@gmail.com>
2018-03-26 11:21:18 -0600

    Some skeleton added

--- Hash.h
+++ Hash.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#define TABLE_SIZE 50
+#define LOW_PRIME 7
+#define HIGH_PRIME 29
+
 //Necessary #includes
+#include "myLinkedList.h"	//FIXME
 #include <string>
 using namespace std;
 
@@ -12,15 +17,16 @@ public:
 	~HashTable();
 
 	void Clear();
-	void Insert(string name, Type data);	//Not detailed enough
-	Type Retrieve(string formula);
+	void Insert(string name, Type data);
+	Type Retrieve(string name);		//Return a Type& ?
+	Type operator [] (string name);	//Notation for Retrieve
 	unsigned int hashFunction(string name);
-	Type operator [] (string name);
 	
 	//Hash tables should be smaller than number of entries to ensure collision
 
 
 private:
+	linkedList<Type>[TABLE_SIZE] data;
 };
 
 
@@ -45,19 +51,31 @@ inline void HashTable<Type>::Insert(string name, Type data)
 }
 
 template<class Type>
-inline Type HashTable<Type>::Retrieve(string formula)
+inline Type HashTable<Type>::Retrieve(string name)
 {
-	return Type();
-}
-
-template<class Type>
-inline unsigned int HashTable<Type>::hashFunction(string name)
-{
-	return 0;
+	unsignsed int index = this->hashFunction(name);
+	linkedList<Type>& hashSlot = this->data[index];
+	ListNode<Type> node = hashSlot.findByName(name);
+	return node.data();
 }
 
 template<class Type>
 inline Type HashTable<Type>::operator[](string name)
 {
-	return Type();
+	Type temp = this->Retrieve(name)
+	return temp;
+}
+
+
+template<class Type>
+inline unsigned int HashTable<Type>::hashFunction(string name)
+{
+	unsigned int index;
+	//FIXME
+	//Convert string to ascii values and add them all together into sum.
+	index *= HIGH_PRIME;
+	index /= LOW_PRIME;	//THIS DOES INTEGER DIVISION RIGHT?! NOTE[]
+	index %= TABLE_SIZE;
+	
+	return index;
 }
